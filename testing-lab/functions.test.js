a5def22baaceae700685d7063f51a6516994bbf6
Boyd Querubin <boydisaacq@gmail.com>
2024-03-21 20:56:33 -0600

    first commit

---
+++ testing-lab/functions.test.js
@@ -0,0 +1,45 @@
+const { returnTwo, greeting, add } = require("../wanted-queries/functions.js");
+
+describe("Formatting Strings", () => {
+  test("returnTwo should return 2", () => {
+    let expected = 2;
+    let actual = returnTwo();
+    expect(actual).toBe(expected);
+  });
+
+  test("greeting should greet with 'Hello,' name. Where name is the value of the name parameter", () => {
+    let name = "James";
+    let expected = `Hello, ${name}`;
+    let actual = greeting(name);
+
+    expect(actual).toBe(expected);
+  });
+
+  test("greeting should greet with 'Hello,' name. Where name is the value of the name parameter", () => {
+    let name = "Jill";
+    let expected = `Hello, ${name}`;
+    let actual = greeting(name);
+
+    expect(actual).toBe(expected);
+  });
+
+  test("add should add num1 and num2", () => {
+    let num1 = 1;
+    let num2 = 2;
+    let expected = num1 + num2;
+
+    let actual = add(num1, num2);
+
+    expect(actual).toBe(expected);
+  });
+
+  test("add should add num1 and num2", () => {
+    let num1 = 5;
+    let num2 = 9;
+    let expected = num1 + num2;
+
+    let actual = add(num1, num2);
+
+    expect(actual).toBe(expected);
+  });
+});
